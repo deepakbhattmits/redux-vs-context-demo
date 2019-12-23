a23f9325efb138a27b0504055a3d33b363d08f63
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-23 15:05:12 +0530

    added new toaster contaiber

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ProductsPage from './pages/Products';
 import CartPage from './pages/Cart';
 import './App.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <ToastContainer autoClose={2000} />
       <Switch>
         <Route path='/' component={ProductsPage} exact />
         <Route path='/cart' component={CartPage} exact />
