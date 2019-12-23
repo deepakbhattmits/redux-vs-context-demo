98fffb788b3974cbf5f6d8b62643cf0c4cca7720
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-23 16:18:00 +0530

    refector

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,13 +1,12 @@
 /** @format */
 
-export const ADD_PRODUCT_TO_CART = 'ADD_PRODUCT_TO_CART';
-export const REMOVE_PRODUCT_FROM_CART = 'REMOVE_PRODCUT_FROM_CART';
+import { shopConstants } from '../constants/type';
 
 export const addProductToCart = product => {
   return dispatch => {
     setTimeout(() => {
       dispatch({
-        type: ADD_PRODUCT_TO_CART,
+        type: shopConstants.ADD_PRODUCT_TO_CART,
         payload: product
       });
     }, 700);
@@ -18,7 +17,7 @@ export const removeProductFromCart = (productId, productTitle) => {
   return dispatch => {
     setTimeout(() => {
       dispatch({
-        type: REMOVE_PRODUCT_FROM_CART,
+        type: shopConstants.REMOVE_PRODUCT_FROM_CART,
         payload: { id: productId, title: productTitle }
       });
     }, 700);
