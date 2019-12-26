e5c7a580e6616f78682c6b7a0c8a51b0cb39b28c
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-26 09:57:00 +0530

    added new layout folder

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,21 @@
+/** @format */
+
+// import React from 'react';
+import React, { useContext } from 'react';
+
+import ShopContext from '../context/shop-context';
+
+import MainNavigation from '../components/MainNavigation';
+const Footer = props => {
+  const context = useContext(ShopContext);
+  return (
+    <div className='footer'>
+      <MainNavigation
+        cartItemNumber={context.cart.reduce((count, curItem) => {
+          return count + curItem.quantity;
+        }, 0)}
+      />
+    </div>
+  );
+};
+export default Footer;
---
+++ src/layout/Layout.jsx
@@ -0,0 +1,15 @@
+/** @format */
+
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+const Layout = props => {
+  return (
+    <>
+      <Header />
+      {props.children}
+      <Footer />
+    </>
+  );
+};
+export default Layout;
