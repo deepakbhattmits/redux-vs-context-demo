508462ee143176db84d228b21d921db5d398e91b
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-26 12:26:37 +0530

    refector

--- src/context/shop-context.js
+++ src/context/shop-context.js
@@ -2,14 +2,14 @@
 
 import { createContext } from 'react';
 export default createContext({
-  products: [
-    { id: 'p1', title: 'Gaming Mouse', price: 29.99 },
-    { id: 'p2', title: 'Harry Potter 3', price: 9.99 },
-    { id: 'p3', title: 'Used plastic bottle', price: 0.99 },
-    { id: 'p4', title: 'Half-dried plant', price: 2.99 }
-  ],
-  cart: [],
-  cartSum: 0,
-  addProductToCart: product => {},
-  removeProductFromCart: productId => {}
+	products: [
+		{ id: 'p1', title: 'Gaming Mouse', price: 29.99 },
+		{ id: 'p2', title: 'Harry Potter 3', price: 9.99 },
+		{ id: 'p3', title: 'Used plastic bottle', price: 0.99 },
+		{ id: 'p4', title: 'Half-dried plant', price: 2.99 }
+	],
+	cart: [],
+	cartSum: 0,
+	addProductToCart: product => {},
+	removeProductFromCart: productId => {}
 });
