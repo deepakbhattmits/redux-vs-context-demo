fbd1077e8870c1b631fa351fa1e130d28d894682
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-26 09:57:15 +0530

    added not found page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,23 @@
+/** @format */
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+const NotFound = props => {
+  // console.log('test : ', props.match.url);
+  return (
+    <main className='not-found'>
+      <div className='not-found-content'>
+        <p>
+          No result found for the search results
+          <strong>{` ${props.match.url.replace('/', '')}`}</strong>
+        </p>
+        <p>
+          <Link className='link' to='/'>
+            <i className='hand point left outline'></i>Go Back..
+          </Link>
+        </p>
+      </div>
+    </main>
+  );
+};
+export default NotFound;
