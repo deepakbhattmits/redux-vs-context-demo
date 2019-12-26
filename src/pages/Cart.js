2afc4bdd38e2447ed795008f76cb9d07c7ccc0e6
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-26 16:36:44 +0530

    refector

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -7,35 +7,35 @@ import ShopContext from '../context/shop-context';
 import './Cart.css';
 
 const CartPage = props => {
-  const context = useContext(ShopContext);
-  return (
-    <>
-      <main className='cart'>
-        {context.cart.length <= 0 && <p>No Item in the Cart!</p>}
-        <ul>
-          {context.cart.map(cartItem => (
-            <li key={cartItem.id}>
-              <div>
-                <strong>{cartItem.title}</strong> - ${cartItem.price} (
-                {cartItem.quantity})
-              </div>
-              <div>
-                <button
-                  onClick={context.removeProductFromCart.bind(
-                    this,
-                    cartItem.id,
-                    cartItem.title
-                  )}
-                >
-                  Remove from Cart
-                </button>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </main>
-    </>
-  );
+	const context = useContext(ShopContext);
+	return (
+		<>
+			<main className='cart'>
+				{context.cart.length <= 0 && <p>No Item in the Cart!</p>}
+				<ul>
+					{context.cart.map(cartItem => (
+						<li key={cartItem.id}>
+							<div>
+								<strong>{cartItem.title}</strong> - ${cartItem.price} (
+								{cartItem.quantity})
+							</div>
+							<div>
+								<button
+									onClick={context.removeProductFromCart.bind(
+										this,
+										cartItem.id,
+										cartItem.title
+									)}
+								>
+									Remove from Cart
+								</button>
+							</div>
+						</li>
+					))}
+				</ul>
+			</main>
+		</>
+	);
 };
 
 export default CartPage;
