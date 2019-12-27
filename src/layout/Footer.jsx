d8053023accc1aacc1dea2b7898864e8f096b77c
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-27 21:54:47 +0530

    refector

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -7,16 +7,16 @@ import ShopContext from '../context/shop-context';
 
 import MainNavigation from '../components/MainNavigation';
 const Footer = props => {
-  // console.log(props)
-  const context = useContext(ShopContext);
-  return (
-    <div className='footer'>
-      <MainNavigation
-        cartItemNumber={context.cart.reduce((count, curItem) => {
-          return count + curItem.quantity;
-        }, 0)}
-      />
-    </div>
-  );
+	// console.log('TEST :',props)
+	const context = useContext(ShopContext);
+	return (
+		<div className='footer'>
+			<MainNavigation
+				cartItemNumber={context.cart.reduce((count, curItem) => {
+					return count + curItem.quantity;
+				}, 0)}
+			/>
+		</div>
+	);
 };
 export default Footer;
