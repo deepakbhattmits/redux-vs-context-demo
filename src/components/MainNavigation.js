a8a6b2f3dd3919b90e1c2639faf7020f55ff078d
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-27 21:54:39 +0530

    refector

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -6,18 +6,19 @@ import { NavLink } from 'react-router-dom';
 import './MainNavigation.css';
 
 const mainNavigation = props => (
-  <header className='main-navigation'>
-    <nav>
-      <ul>
-        <li>
-          <NavLink to='/'>Products</NavLink>
-        </li>
-        <li>
-          <NavLink to='/cart'>Cart ({props.cartItemNumber})</NavLink>
-        </li>
-      </ul>
-    </nav>
-  </header>
+	// console.log(props)
+	<header className='main-navigation'>
+		<nav>
+			<ul>
+				<li>
+					<NavLink to='/'>Products</NavLink>
+				</li>
+				<li>
+					<NavLink to='/cart'>Cart ({props.cartItemNumber})</NavLink>
+				</li>
+			</ul>
+		</nav>
+	</header>
 );
 
 export default mainNavigation;
