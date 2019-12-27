23ef67cfca4bbbd1788d29cdedfe42f5dcb3e651
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-27 21:52:24 +0530

    refector

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,23 +3,24 @@
 import { shopConstants } from '../constants/type';
 
 export const addProductToCart = product => {
-  return dispatch => {
-    setTimeout(() => {
-      dispatch({
-        type: shopConstants.ADD_PRODUCT_TO_CART,
-        payload: product
-      });
-    }, 700);
-  };
+	// console.log(product)
+	return dispatch => {
+		setTimeout(() => {
+			dispatch({
+				type: shopConstants.ADD_PRODUCT_TO_CART,
+				payload: product
+			});
+		}, 700);
+	};
 };
 
 export const removeProductFromCart = (productId, productTitle) => {
-  return dispatch => {
-    setTimeout(() => {
-      dispatch({
-        type: shopConstants.REMOVE_PRODUCT_FROM_CART,
-        payload: { id: productId, title: productTitle }
-      });
-    }, 700);
-  };
+	return dispatch => {
+		setTimeout(() => {
+			dispatch({
+				type: shopConstants.REMOVE_PRODUCT_FROM_CART,
+				payload: { id: productId, title: productTitle }
+			});
+		}, 700);
+	};
 };
