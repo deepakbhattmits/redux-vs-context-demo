94ad2681da18ac7db5e951f9c3aa7e0de2374e87
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-27 21:52:33 +0530

    refector

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -5,37 +5,38 @@ import ShopContext from '../context/shop-context';
 import './Products.css';
 
 const ProductsPage = props => {
-  return (
-    <ShopContext.Consumer>
-      {context => (
-        <>
-          <main className='products'>
-            <ul>
-              {context.products.map(product => (
-                <li key={product.id}>
-                  <div>
-                    <img
-                      src='https://via.placeholder.com/150'
-                      alt={product.title}
-                    />
-                  </div>
-                  <div>
-                    <strong>{product.title}</strong> - ${product.price}
-                  </div>
-                  <div>
-                    <button
-                      onClick={context.addProductToCart.bind(this, product)}
-                    >
-                      Add to Cart
-                    </button>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </main>
-        </>
-      )}
-    </ShopContext.Consumer>
-  );
+	// console.log('TEST props : ', props);
+	return (
+		<ShopContext.Consumer>
+			{context => (
+				<>
+					<main className='products'>
+						<ul>
+							{context.products.map(product => (
+								<li key={product.id}>
+									<div>
+										<img
+											src='https://via.placeholder.com/150'
+											alt={product.title}
+										/>
+									</div>
+									<div>
+										<strong>{product.title}</strong> - ${product.price}
+									</div>
+									<div>
+										<button
+											onClick={context.addProductToCart.bind(this, product)}
+										>
+											Add to Cart
+										</button>
+									</div>
+								</li>
+							))}
+						</ul>
+					</main>
+				</>
+			)}
+		</ShopContext.Consumer>
+	);
 };
 export default ProductsPage;
