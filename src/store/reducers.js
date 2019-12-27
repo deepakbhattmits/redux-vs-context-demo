18d89af475f5fd0a9283032b96ff3601d6a4b59b
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-27 21:52:15 +0530

    refector

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -4,69 +4,70 @@ import { shopConstants } from '../constants/type';
 import { toast } from 'react-toastify';
 
 const initialState = {
-  products: [
-    { id: 'p1', title: 'Gaming Mouse', price: 29.99 },
-    { id: 'p2', title: 'Harry Potter 3', price: 9.99 },
-    { id: 'p3', title: 'Used plastic bottle', price: 0.99 },
-    { id: 'p4', title: 'Half-dried plant', price: 2.99 }
-  ],
-  cart: [],
-  cartSum: 0
+	products: [
+		{ id: 'p1', title: 'Gaming Mouse', price: 29.99 },
+		{ id: 'p2', title: 'Harry Potter 3', price: 9.99 },
+		{ id: 'p3', title: 'Used plastic bottle', price: 0.99 },
+		{ id: 'p4', title: 'Half-dried plant', price: 2.99 }
+	],
+	cart: [],
+	cartSum: 0
 };
 
 const shopReducer = (state = initialState, action) => {
-  // const options = {
-  //   onOpen: props => console.log('open'),
-  //   onClose: props => console.log('close'),
-  //   autoClose: 6000,
-  //   closeButton: <FontAwesomeCloseButton />,
-  //   type: toast.TYPE.INFO,
-  //   hideProgressBar: false,
-  //   position: toast.POSITION.TOP_RIGHT,
-  //   pauseOnHover: true,
-  //   transition: MyCustomTransition,
-  //   progress: 0.2
-  // };
-  let updatedCart;
-  let updatedItemIndex;
-  switch (action.type) {
-    case shopConstants.ADD_PRODUCT_TO_CART:
-      updatedCart = [...state.cart];
-      updatedItemIndex = updatedCart.findIndex(
-        item => item.id === action.payload.id
-      );
+	// const options = {
+	//   onOpen: props => console.log('open'),
+	//   onClose: props => console.log('close'),
+	//   autoClose: 6000,
+	//   closeButton: <FontAwesomeCloseButton />,
+	//   type: toast.TYPE.INFO,
+	//   hideProgressBar: false,
+	//   position: toast.POSITION.TOP_RIGHT,
+	//   pauseOnHover: true,
+	//   transition: MyCustomTransition,
+	//   progress: 0.2
+	// };
+	let updatedCart;
+	let updatedItemIndex;
+	switch (action.type) {
+		case shopConstants.ADD_PRODUCT_TO_CART:
+			// console.log(action.type)
+			updatedCart = [...state.cart];
+			updatedItemIndex = updatedCart.findIndex(
+				item => item.id === action.payload.id
+			);
 
-      if (updatedItemIndex < 0) {
-        updatedCart.push({ ...action.payload, quantity: 1 });
-      } else {
-        const updatedItem = {
-          ...updatedCart[updatedItemIndex]
-        };
-        updatedItem.quantity += 1;
-        updatedCart[updatedItemIndex] = updatedItem;
-      }
-      toast.success(`Added Product: ${action.payload.title}`);
-      return { ...state, cart: updatedCart };
-    case shopConstants.REMOVE_PRODUCT_FROM_CART:
-      updatedCart = [...state.cart];
-      updatedItemIndex = updatedCart.findIndex(
-        item => item.id === action.payload.id
-      );
+			if (updatedItemIndex < 0) {
+				updatedCart.push({ ...action.payload, quantity: 1 });
+			} else {
+				const updatedItem = {
+					...updatedCart[updatedItemIndex]
+				};
+				updatedItem.quantity += 1;
+				updatedCart[updatedItemIndex] = updatedItem;
+			}
+			toast.success(`Added Product: ${action.payload.title}`);
+			return { ...state, cart: updatedCart };
+		case shopConstants.REMOVE_PRODUCT_FROM_CART:
+			updatedCart = [...state.cart];
+			updatedItemIndex = updatedCart.findIndex(
+				item => item.id === action.payload.id
+			);
 
-      const updatedItem = {
-        ...updatedCart[updatedItemIndex]
-      };
-      updatedItem.quantity -= 1;
-      if (updatedItem.quantity <= 0) {
-        updatedCart.splice(updatedItemIndex, 1);
-      } else {
-        updatedCart[updatedItemIndex] = updatedItem;
-      }
-      toast.error(`Removed Product: ${action.payload.title}`);
-      return { ...state, cart: updatedCart };
-    default:
-      return state;
-  }
+			const updatedItem = {
+				...updatedCart[updatedItemIndex]
+			};
+			updatedItem.quantity -= 1;
+			if (updatedItem.quantity <= 0) {
+				updatedCart.splice(updatedItemIndex, 1);
+			} else {
+				updatedCart[updatedItemIndex] = updatedItem;
+			}
+			toast.error(`Removed Product: ${action.payload.title}`);
+			return { ...state, cart: updatedCart };
+		default:
+			return state;
+	}
 };
 
 export default shopReducer;
