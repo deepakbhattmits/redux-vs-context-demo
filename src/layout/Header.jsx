2c2d5f9a39f2700a901c6d20a4452205e417cc95
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-27 21:54:12 +0530

    refector

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -7,16 +7,16 @@ import ShopContext from '../context/shop-context';
 
 import MainNavigation from '../components/MainNavigation';
 const Header = props => {
-  // console.log(props)
-  const context = useContext(ShopContext);
-  return (
-    <div className='header'>
-      <MainNavigation
-        cartItemNumber={context.cart.reduce((count, curItem) => {
-          return count + curItem.quantity;
-        }, 0)}
-      />
-    </div>
-  );
+	// console.log('TEST :',props)
+	const context = useContext(ShopContext);
+	return (
+		<div className='header'>
+			<MainNavigation
+				cartItemNumber={context.cart.reduce((count, curItem) => {
+					return count + curItem.quantity;
+				}, 0)}
+			/>
+		</div>
+	);
 };
 export default Header;
