cda0d8edd57506e7e7df2346178d601c701d1f51
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-28 07:42:35 +0530

    refector

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -7,7 +7,7 @@ import ShopContext from '../context/shop-context';
 
 import MainNavigation from '../components/MainNavigation';
 const Footer = props => {
-	// console.log('TEST :',props)
+	// console.log('props :',props)
 	const context = useContext(ShopContext);
 	return (
 		<div className='footer'>
