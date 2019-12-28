0ea93847d52a31cb90dafdd0cc5dd955969019bc
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-28 07:44:03 +0530

    REFECTOR

--- src/index.js
+++ src/index.js
@@ -12,11 +12,12 @@ import shopReducer from './store/reducers';
 
 const store = createStore(shopReducer, applyMiddleware(reduxThunk));
 const rootElement = document.querySelector('#root');
+// console.log('test : ', rootElement);
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  rootElement
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	rootElement
 );
 
 // If you want your app to work offline and load faster, you can change
